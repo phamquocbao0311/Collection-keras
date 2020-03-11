f85073862d041562ba35cf3e0f8c96886fdb2e73
phamquocbao0311 <59155979+phamquocbao0311@users.noreply.github.com>
2020-03-11 15:41:54 +0700

    Update ohem.py

--- losses/ohem/ohem.py
+++ losses/ohem/ohem.py
@@ -1,5 +1,6 @@
 def ohem_loss(ytrue, ypred):
-    result = keras.losses.categorical_crossentropy(ytrue, ypred, label_smoothing=0)
+    result = K.categorical_crossentropy(ytrue, ypred, axis=-1)
     alpha = K.variable(0.1, dtype=tf.float32)
     index = K.greater(result, alpha)
-    return K.mean(result[index])
+    cast = tf.cast(index, tf.float32)
+    return result*cast
